b63b911486b71e7026d17dbdfd55b6f8c473102c
sebastianAbello <juanse.abello.2004@gmail.com>
2024-01-13 18:07:33 -0500

    init moduls

---
+++ 08-CursoJS/2_Modulos/2-modulos.js
@@ -0,0 +1,11 @@
+import { pi, usuario } from "./3-constantes.js";
+import saludar from "./3-constantes.js";
+import { aritmetica } from "./4-aritmetica.js";
+
+console.log("Archivo modulos.js");
+
+console.log(pi, usuario);
+
+console.log(aritmetica.sumar(2, 2));
+
+saludar();
---
+++ 08-CursoJS/2_Modulos/3-constantes.js
@@ -0,0 +1,9 @@
+export const pi = Math.PI;
+
+export let usuario = "sebastian";
+let password = "queca";
+
+//Solo se puede hacer una exportacio default
+export default function saludar() {
+  console.log("Hola modulos +ES6");
+}
